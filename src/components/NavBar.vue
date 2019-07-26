954d81500a888b465395e8b7820f4a93254a9b3e
Ben Jefferies <benjefferies@echosoft.uk>
2019-07-26 12:39:42 +0100

    Update deps and remove about

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,23 +2,13 @@
   <div>
     <b-navbar class="navbar" toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand href="#" class="brand">echosoft</b-navbar-brand>
-      <b-navbar-nav>
-        <b-nav-item @click="scrollDown()">About</b-nav-item>
-      </b-navbar-nav>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
-  methods: {
-    scrollDown: () => {
-      console.log('test')
-    }
-  }
+  name: 'NavBar'
 }
 </script>
 
