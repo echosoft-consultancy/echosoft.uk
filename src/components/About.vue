d1daf689a41a96a00e2da7b922137b1d110fbda5
Ben Jefferies <benjefferies@echosoft.uk>
2019-11-27 11:08:43 +0000

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,7 +10,7 @@
          <p>Hi, I'm Ben Jefferies, a freelance DevOps engineer with expertise in Web Application Development and Continous Deployment.</p>
          <p>I entered the industry through a less common route in that I didn't graduate from University, however, I was offered an apprenticeship with Nominet which gave me the jump start I needed to pursue my passion in the realm of technology where I gained valuable hands on knowledge as well as gaining industry recognised qualifications.</p>
          <p>Now, 9 years on, I have experience in implementing complex software solutions for a variety of industries varying from Public Sector, Financial to ECommerce.</p>
-         <p>In my spare time I like to investigate and play with new technologies (check out my projects on GitHub) and gadgets as well as hiking and dog walking!</p>
+         <p>In my spare time I like to investigate and play with new technologies (check out my projects on GitHub) and gadgets as well as hiking, dog walking and <a href="https://www.youtube.com/channel/UC3ub6tcMx7teVM65BU2IE6w?view_as=subscriber">drone videography</a>!</p>
          <p>I am currently available to consult or for fixed price work.</p>
       </b-col>
     </b-row>
